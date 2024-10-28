e443f19e3f751fcaa5dc44c6ff631f36fa297df3
hduoc2003 <hduoc2003@gmail.com>
2024-10-28 04:35:09 +0000

    remove redundant code

--- sp1-prover/src/lib.rs
+++ sp1-prover/src/lib.rs
@@ -43,65 +43,13 @@ pub fn calc_auction_hash(auction_data: &AuctionData) -> [u8; 32] {
 #[cfg(test)]
 mod tests {
     use std::fs;
-    use std::fs::File;
-    use std::io::Read;
-    use std::process::Command;
 
     use ecies::{PublicKey, SecretKey};
     use rand::rngs::OsRng;
-    // use rsa::pkcs8::{DecodePrivateKey, EncodePrivateKey, EncodePublicKey, LineEnding};
-    // use rsa::rand_core::OsRng;
-    // use rsa::{Pkcs1v15Encrypt, RsaPrivateKey, RsaPublicKey};
-    use sp1_sdk::{ProverClient, SP1Stdin};
 
     use crate::{calc_auction_hash, AuctionData, Bidder};
-    // use ecies_ed25519::generate_keypair;
-    // use rand::{CryptoRng, RngCore};
 
     const PVK_HEX: &str = include_str!("../private_encryption_key");
-    
-    #[test]
-    fn test_sp1_prover() {
-        // compile main
-        let output = Command::new("cargo")
-            .args(["prove", "build"])
-            .output()
-            .unwrap();
-        println!("{:?}", String::from_utf8_lossy(output.stdout.as_slice()));
-        let elf = {
-            let mut buffer = Vec::new();
-            File::open("./elf/riscv32im-succinct-zkvm-elf")
-                .unwrap()
-                .read_to_end(&mut buffer)
-                .unwrap();
-            buffer
-        };
-
-        // let mut rng = rand::thread_rng();
-        let (_, pbk) = get_key();
-
-        let mut stdin = SP1Stdin::new();
-        stdin.write(&auction_data(&pbk));
-
-        let client = ProverClient::new();
-        let (pk, vk) = client.setup(elf.as_slice());
-
-        let Ok(mut proof) = client.prove(&pk, stdin).run() else {
-            println!("Something went wrong!");
-            return;
-        };
-
-        println!("Proof generated successfully. Verifying proof...");
-        client.verify(&proof, &vk).expect("verification failed");
-        println!("Proof verified successfully.");
-
-        // println!("{:?}", proof.public_values);
-        let hash_data = proof.public_values.read::<[u8; 32]>();
-        println!("{:?}", hash_data);
-        let winner_addr = proof.public_values.read::<Vec<u8>>();
-        println!("{:?}", winner_addr);
-        // Todo: validate with data
-    }
 
     #[test]
     fn test_decrypt_data() {
